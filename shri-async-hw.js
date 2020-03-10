83e7a40aa37b5855cb1c7bdf8078d01be9571096
Dmitry Andriyanov <dima117a@yandex-team.ru>
2020-03-10 15:47:06 +0300

    async api

---
+++ shri-async-hw.js
@@ -0,0 +1,41 @@
+(() => {
+
+    const _wrap = (fn, cb) => {
+        setTimeout(() => { 
+            cb(fn());
+        }, Math.random() * 20);
+    };
+    
+    const AsyncArray = function(initial) {
+        if (initial && !(initial instanceof Array)) {
+            throw new Error('initial value is not an array');
+        }
+    
+        const a = initial ? Array.from(initial) : [];
+    
+        this.set = (index, value, cb) => _wrap(() => { a[index] = value }, cb);
+        this.push = (value, cb) => _wrap(() => { a.push(value) }, cb);
+    
+        this.get = (index, cb) => _wrap(() => a[index], cb);
+        this.pop = (cb) => _wrap(() => a.pop(), cb);
+        this.length = (cb) => _wrap(() => a.length, cb);
+
+        this.print = () => { console.log(a.toString()); };
+    }
+    
+    const add = (a, b, cb) => _wrap(() => a + b, cb);
+    const subtract = (a, b, cb) => _wrap(() => a - b, cb);
+    const multiply = (a, b, cb) => _wrap(() => a * b, cb);
+    const divide = (a, b, cb) => _wrap(() => a / b, cb);
+
+    window.Homework = {
+        AsyncArray,
+        add,
+        subtract,
+        multiply,
+        divide
+    };
+
+    Object.freeze(window.Homework);
+})();
+
