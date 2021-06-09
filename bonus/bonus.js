2f92d0ecbbcc079313e023f47724139f502bc257
vmik7 <vmik7@yandex.ru>
2021-06-09 12:40:14 +0300

    Реализовано бонусное задание

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -1,3 +1,58 @@
 Promise._any = function(promises) {
-    
+    return new Promise((resolve, reject) => {
+        
+        let pendingCount = promises.length;
+        let findFulfilled = false;
+        let errors = [];
+
+        promises.forEach((p) => {
+            p.then(res => {
+                if (!findFulfilled) {
+                    findFulfilled = true;
+                    resolve(res);
+                }
+            }, (e) => {
+                errors.push(e);
+                pendingCount--;
+                if (pendingCount === 0) {
+                    throw new AggregateError(errors, 'All Promises rejected');
+                }
+            });
+        }); 
+    });
+}
+
+Promise._allSettled = function(promises) {
+    return new Promise((resolve, reject) => {
+        
+        let pendingCount = promises.length;
+        let results = [];
+
+        promises.forEach((p) => {
+            p.then(res => {
+                results.push({
+                    status: 'fulfilled',
+                    value: res
+                });
+                pendingCount--;
+                if (pendingCount === 0) {
+                    resolve(results);
+                }
+            }, e => {
+                results.push({
+                    status: 'rejected',
+                    reason: e
+                });
+                pendingCount--;
+                if (pendingCount === 0) {
+                    resolve(results);
+                }
+            });
+        }); 
+    });
+}
+
+Promise.prototype._finally = function(onFinally) {
+    this.then(onFinally, onFinally);
+    return this;
 }
